47e549cc97a4e798f65c91b9ebfaa5518576c4bf
kirakishou <kez1911@protonmail.com>
2017-07-25 14:42:55 +0300

    added missing file

---
+++ app/src/main/java/com/kirakishou/fixmypc/fixmypcapp/mvp/model/ServiceMessageType.kt
@@ -0,0 +1,8 @@
+package com.kirakishou.fixmypc.fixmypcapp.mvp.model
+
+/**
+ * Created by kirakishou on 7/25/2017.
+ */
+enum class ServiceMessageType(val value: Int) {
+    SERVICE_MESSAGE_LOGIN(0)
+}

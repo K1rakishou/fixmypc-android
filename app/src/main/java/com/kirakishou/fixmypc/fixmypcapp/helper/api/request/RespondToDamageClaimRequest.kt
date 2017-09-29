a0d42179a459bf524f79f2c214a7812df7762305
kirakishou <kez1911@protonmail.com>
2017-09-29 17:37:00 +0300

    RespondToDamageClaimRequest now handles re-login on SESSION_ID_EXPIRED server errorCode

--- app/src/main/java/com/kirakishou/fixmypc/fixmypcapp/helper/api/request/RespondToDamageClaimRequest.kt
+++ app/src/main/java/com/kirakishou/fixmypc/fixmypcapp/helper/api/request/RespondToDamageClaimRequest.kt
@@ -2,15 +2,20 @@ package com.kirakishou.fixmypc.fixmypcapp.helper.api.request
 
 import com.google.gson.Gson
 import com.kirakishou.fixmypc.fixmypcapp.helper.api.ApiService
+import com.kirakishou.fixmypc.fixmypcapp.helper.rx.operator.OnApiErrorObservable
 import com.kirakishou.fixmypc.fixmypcapp.helper.rx.operator.OnApiErrorSingle
 import com.kirakishou.fixmypc.fixmypcapp.helper.rx.scheduler.SchedulerProvider
 import com.kirakishou.fixmypc.fixmypcapp.mvvm.model.AppSettings
 import com.kirakishou.fixmypc.fixmypcapp.mvvm.model.ErrorCode
+import com.kirakishou.fixmypc.fixmypcapp.mvvm.model.entity.request.LoginPacket
 import com.kirakishou.fixmypc.fixmypcapp.mvvm.model.entity.request.RespondToDamageClaimPacket
 import com.kirakishou.fixmypc.fixmypcapp.mvvm.model.entity.response.RespondToDamageClaimResponse
+import com.kirakishou.fixmypc.fixmypcapp.mvvm.model.entity.response.StatusResponse
 import com.kirakishou.fixmypc.fixmypcapp.mvvm.model.exceptions.ApiException
 import com.kirakishou.fixmypc.fixmypcapp.mvvm.model.exceptions.BadServerResponseException
+import com.kirakishou.fixmypc.fixmypcapp.mvvm.model.exceptions.CouldNotUpdateSessionId
 import com.kirakishou.fixmypc.fixmypcapp.mvvm.model.exceptions.UserInfoIsEmpty
+import io.reactivex.Observable
 import io.reactivex.Single
 import java.net.UnknownHostException
 import java.util.concurrent.TimeoutException
@@ -29,11 +34,48 @@ class RespondToDamageClaimRequest(protected val packet: RespondToDamageClaimPack
             throw UserInfoIsEmpty()
         }
 
-        //TODO: handle sessionId expiring
         return mApiService.respondToDamageClaim(mAppSettings.loadUserInfo().sessionId, packet)
                 .subscribeOn(mSchedulers.provideIo())
                 .lift(OnApiErrorSingle(mGson))
+                .flatMap { response ->
+                    if (response.errorCode == ErrorCode.Remote.REC_SESSION_ID_EXPIRED) {
+                        return@flatMap reLoginAndResendRequest()
+                    }
+
+                    return@flatMap Single.just(response)
+                }
                 .onErrorResumeNext { error -> exceptionToErrorCode(error) }
+    }
+
+    private fun reLoginAndResendRequest(): Single<RespondToDamageClaimResponse> {
+        if (!mAppSettings.isUserInfoExists()) {
+            throw UserInfoIsEmpty()
+        }
+
+        val userInfo = mAppSettings.loadUserInfo()
+
+        val loginResponseObservable = mApiService.doLogin(LoginPacket(userInfo.login, userInfo.password))
+                .subscribeOn(mSchedulers.provideIo())
+                .lift(OnApiErrorSingle(mGson))
+                .toObservable()
+                .publish()
+                .autoConnect(2)
+
+        val successObservable = loginResponseObservable
+                .filter { it.errorCode == ErrorCode.Remote.REC_OK }
+                .doOnNext { mAppSettings.updateSessionId(it.sessionId) }
+                .flatMap {
+                    return@flatMap mApiService.respondToDamageClaim(mAppSettings.loadUserInfo().sessionId, packet).toObservable()
+                }
+                .lift<RespondToDamageClaimResponse>(OnApiErrorObservable(mGson))
+
+        val failObservable = loginResponseObservable
+                .filter { it.errorCode != ErrorCode.Remote.REC_OK }
+                .doOnNext { throw CouldNotUpdateSessionId() }
+                .map { RespondToDamageClaimResponse(it.errorCode) }
+
+        return Observable.merge(successObservable, failObservable)
+                .single(StatusResponse(ErrorCode.Remote.REC_EMPTY_OBSERVABLE_ERROR) as RespondToDamageClaimResponse)
     }
 
     private fun exceptionToErrorCode(error: Throwable): Single<RespondToDamageClaimResponse> {
